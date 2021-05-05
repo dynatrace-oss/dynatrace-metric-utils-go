deea64625adad7cd67b3a5f232043b8bf69f2ab3
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2021-05-05 17:50:55 +0200

    drop line length constant and rename line limit

--- metric/apiconstants/apiconstants.go
+++ metric/apiconstants/apiconstants.go
@@ -2,8 +2,7 @@ package apiconstants
 
 const (
 	defaultOneAgentEndpoint = "http://127.0.0.1:14499/metrics/ingest"
-	lineLengthLimit         = 2000
-	metricPayloadLinesLimit = 1000
+	payloadLinesLimit       = 1000
 )
 
 // GetDefaultOneAgentEndpoint returns the default OneAgent metrics ingest endpoint.
@@ -11,12 +10,7 @@ func GetDefaultOneAgentEndpoint() string {
 	return defaultOneAgentEndpoint
 }
 
-// GetLineLengthLimit returns the maximum number of characters allowed per metric line.
-func GetLineLengthLimit() int {
-	return lineLengthLimit
-}
-
 // GetMetricPayloadLinesLimit returns the maximum number of lines per POST request.
-func GetMetricPayloadLinesLimit() int {
-	return metricPayloadLinesLimit
+func GetPayloadLinesLimit() int {
+	return payloadLinesLimit
 }
